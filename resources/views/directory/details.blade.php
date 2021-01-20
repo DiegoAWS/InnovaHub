0e98bd9d6e32cf11bf2855ddfe536888775e9e28
Diego Escobar <diego.netsolutions@gmail.com>
2021-01-19 22:21:04 -0500

    Details View implement 90%

--- resources/views/directory/details.blade.php
+++ resources/views/directory/details.blade.php
@@ -1,6 +1,5 @@
 <div class="auto-container mx-xs-1 mx-sm-2 mx-md-3">
-    <a href="/directory" class="btn btn-primary detailsBackButton" data-navigo><i
-            class="fa fa-chevron-circle-left fa-xl" aria-hidden="true"></i></a>
+    <a href="/directory" class="btn btn-primary detailsBackButton" data-navigo><i class="fa fa-chevron-circle-left fa-xl" aria-hidden="true"></i></a>
     <div class="row clearfix">
         <div class="flex detailsSubContainer">
             <div class="w-60 smallFull p-4">
@@ -16,23 +15,88 @@
             </div>
         </div>
         <div class="row">
-            <div class=" col-md-3 col-sm-4 col-xs-12">
+            <div class="col-xs-12 col-sm-6 col-md-8 ">
                 <div class="sidebar-widget brouchure-widget mt-5">
-                    <div class="inner-box">
-                        <h2>Download Brochures</h2>
-                        <ul class="boucher">
-                            <li>
-                                <h5><span>Pdf</span>Research Results<i class="fa fa-download"></i></h5>
-                            </li>
-                            <li>
-                                <h5><span>Doc</span>Service Brochure<i class="fa fa-download"></i></h5>
-                            </li>
-                        </ul>
+
+                    <div class="row ">
+                        <div class="col-xs-12 col-sm-4  flex align-center">
+                            <h3 class="text-muted mt-5">
+                                Documentación
+                            </h3>
+                        </div>
+
+                        <div class="col-xs-11 col-sm-7">
+                            <div class="inner-box">
+                                <h2>Download Brochures</h2>
+                                <ul class="boucher">
+                                    <li>
+                                        <h5><span>Pdf</span>Research Results<i class="fa fa-download"></i></h5>
+                                    </li>
+                                    <li>
+                                        <h5><span>Doc</span>Service Brochure<i class="fa fa-download"></i></h5>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-xs-9">
+                            <table class="detailsTable">
+                                <tr>
+                                    <th class="keyTable"></th>
+                                    <th class="colonTable"></th>
+                                    <th class="valueTable"></th>
+                                </tr>
+                                <tr>
+                                    <td class="text-primary celdaTable">Sector</td>
+                                    <td>:</td>
+                                    <td>Infocomunicaciones / Servicios Tecnológicos y empresariales para el Turismo</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-primary celdaTable">Sector</td>
+                                    <td>:</td>
+                                    <td>Infocomunicaciones / Servicios Tecnológicos</td>
+                                </tr>
+                                <tr>
+                                    <td class="text-primary celdaTable">Sector</td>
+                                    <td>:</td>
+                                    <td>Infocomunicaciones / Servicios Tecnológicos</td>
+                                </tr>
+
+                            </table>
+                        </div>
+                        <div class="col-xs-2 iconDetailsContainer">
+                            <div class="flex">
+                                <div class="handShakeIcon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z" />
+                                    </svg>
+                                </div>
+                                <div class="iconValueDetails">
+                                    30
+                                </div>
+                            </div>
+
+                            <div class="flex mt-2">
+                                <div class="storeIcon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path d="M12 192h424c6.6 0 12 5.4 12 12v260c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V204c0-6.6 5.4-12 12-12zm436-44v-36c0-26.5-21.5-48-48-48h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v36c0 6.6 5.4 12 12 12h424c6.6 0 12-5.4 12-12z" />
+                                    </svg>
+                                </div>
+                                <div class="iconValueDetails">
+                                    10
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
+
+
                 </div>
+
             </div>
 
-            <div class="sidebar-side pull-right col-md-3 col-sm-4 col-xs-12">
+            <div class="sidebar-side pull-right col-md-4 col-sm-6 col-xs-12">
                 <aside class="sidebar">
 
 
@@ -49,9 +113,21 @@
                             <a href="#" class="contact-btn theme-btn btn-style-three">Get Quote free</a>
                         </div>
                     </div>
-
+                    <div class="flex">
+                        <div class="handShakeIcon mx-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="white">
+                                <path d="M519.2 127.9l-47.6-47.6A56.252 56.252 0 0 0 432 64H205.2c-14.8 0-29.1 5.9-39.6 16.3L118 127.9H0v255.7h64c17.6 0 31.8-14.2 31.9-31.7h9.1l84.6 76.4c30.9 25.1 73.8 25.7 105.6 3.8 12.5 10.8 26 15.9 41.1 15.9 18.2 0 35.3-7.4 48.8-24 22.1 8.7 48.2 2.6 64-16.8l26.2-32.3c5.6-6.9 9.1-14.8 10.9-23h57.9c.1 17.5 14.4 31.7 31.9 31.7h64V127.9H519.2zM48 351.6c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16c0 8.9-7.2 16-16 16zm390-6.9l-26.1 32.2c-2.8 3.4-7.8 4-11.3 1.2l-23.9-19.4-30 36.5c-6 7.3-15 4.8-18 2.4l-36.8-31.5-15.6 19.2c-13.9 17.1-39.2 19.7-55.3 6.6l-97.3-88H96V175.8h41.9l61.7-61.6c2-.8 3.7-1.5 5.7-2.3H262l-38.7 35.5c-29.4 26.9-31.1 72.3-4.4 101.3 14.8 16.2 61.2 41.2 101.5 4.4l8.2-7.5 108.2 87.8c3.4 2.8 3.9 7.9 1.2 11.3zm106-40.8h-69.2c-2.3-2.8-4.9-5.4-7.7-7.7l-102.7-83.4 12.5-11.4c6.5-6 7-16.1 1-22.6L367 167.1c-6-6.5-16.1-6.9-22.6-1l-55.2 50.6c-9.5 8.7-25.7 9.4-34.6 0-9.3-9.9-8.5-25.1 1.2-33.9l65.6-60.1c7.4-6.8 17-10.5 27-10.5l83.7-.2c2.1 0 4.1.8 5.5 2.3l61.7 61.6H544v128zm48 47.7c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16c0 8.9-7.2 16-16 16z" />
+                            </svg>
+                        </div>
+                        <div class="handShakeIcon mx-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="white">
+                                <path d="M519.2 127.9l-47.6-47.6A56.252 56.252 0 0 0 432 64H205.2c-14.8 0-29.1 5.9-39.6 16.3L118 127.9H0v255.7h64c17.6 0 31.8-14.2 31.9-31.7h9.1l84.6 76.4c30.9 25.1 73.8 25.7 105.6 3.8 12.5 10.8 26 15.9 41.1 15.9 18.2 0 35.3-7.4 48.8-24 22.1 8.7 48.2 2.6 64-16.8l26.2-32.3c5.6-6.9 9.1-14.8 10.9-23h57.9c.1 17.5 14.4 31.7 31.9 31.7h64V127.9H519.2zM48 351.6c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16c0 8.9-7.2 16-16 16zm390-6.9l-26.1 32.2c-2.8 3.4-7.8 4-11.3 1.2l-23.9-19.4-30 36.5c-6 7.3-15 4.8-18 2.4l-36.8-31.5-15.6 19.2c-13.9 17.1-39.2 19.7-55.3 6.6l-97.3-88H96V175.8h41.9l61.7-61.6c2-.8 3.7-1.5 5.7-2.3H262l-38.7 35.5c-29.4 26.9-31.1 72.3-4.4 101.3 14.8 16.2 61.2 41.2 101.5 4.4l8.2-7.5 108.2 87.8c3.4 2.8 3.9 7.9 1.2 11.3zm106-40.8h-69.2c-2.3-2.8-4.9-5.4-7.7-7.7l-102.7-83.4 12.5-11.4c6.5-6 7-16.1 1-22.6L367 167.1c-6-6.5-16.1-6.9-22.6-1l-55.2 50.6c-9.5 8.7-25.7 9.4-34.6 0-9.3-9.9-8.5-25.1 1.2-33.9l65.6-60.1c7.4-6.8 17-10.5 27-10.5l83.7-.2c2.1 0 4.1.8 5.5 2.3l61.7 61.6H544v128zm48 47.7c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16c0 8.9-7.2 16-16 16z" />
+                            </svg>
+                        </div>
+                    </div>
                 </aside>
             </div>
         </div>
+
     </div>
 </div>
